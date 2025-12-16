1706582bf06eb4dc37197173d6292b3c0aba1b94
nassaigael <hei.nassai@gmail.com>
2025-12-16 22:21:39 +0300

    Delete Comment on DistrictController

--- api/src/main/java/mg/fizanakara/api/controllers/DistrictController.java
+++ api/src/main/java/mg/fizanakara/api/controllers/DistrictController.java
@@ -49,7 +49,7 @@ public class DistrictController {
     // UPDATE
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<District> updateDistrict(@PathVariable Long id, @RequestBody @Validated DistrictDto dto) {  // ← AJOUT : @Validated
+    public ResponseEntity<District> updateDistrict(@PathVariable Long id, @RequestBody @Validated DistrictDto dto) {
         log.info("Mise à jour du district ID {} : {}", id, dto.getName());
         District updated = districtService.updateDistrict(id, dto);
         return ResponseEntity.ok(updated);

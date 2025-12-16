71b7c1ebe42bf163376987d69d34930aedc3a8d4
nassaigael <hei.nassai@gmail.com>
2025-12-16 20:13:47 +0300

    add DistrictRepo classe on package services:: Interface implement crud

--- api/src/main/java/mg/fizanakara/api/services/DistrictService.java
+++ api/src/main/java/mg/fizanakara/api/services/DistrictService.java
@@ -1,4 +1,16 @@
 package mg.fizanakara.api.services;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mg.fizanakara.api.repository.DistrictRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class DistrictService {
+    private final DistrictRepository districtRepository;
+
+    // GET ALL
+
 }

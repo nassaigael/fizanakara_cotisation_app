11d17724c9c8472774f5736bb2adeb2316bcd4f8
nassaigael <hei.nassai@gmail.com>
2025-12-16 20:15:01 +0300

    implement methode get all districts

--- api/src/main/java/mg/fizanakara/api/services/DistrictService.java
+++ api/src/main/java/mg/fizanakara/api/services/DistrictService.java
@@ -12,5 +12,9 @@ public class DistrictService {
     private final DistrictRepository districtRepository;
 
     // GET ALL
+    public List<District> getAllDistricts() {
+        return districtRepository.findAll();
+    }
 
+    
 }

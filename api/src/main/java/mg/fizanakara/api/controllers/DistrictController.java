43ff20ecbfd369a2c5f19bfbd99999a5aef6efe9
nassaigael <hei.nassai@gmail.com>
2025-12-16 20:39:57 +0300

    Implements:: GET ALL => getAllDistricts

--- api/src/main/java/mg/fizanakara/api/controllers/DistrictController.java
+++ api/src/main/java/mg/fizanakara/api/controllers/DistrictController.java
@@ -1,4 +1,12 @@
 package mg.fizanakara.api.controllers;
 
 public class DistrictController {
+    private final DistrictService districtService;
+
+    // GET ALL
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<District>> getAllDistricts(@PathVariable Long id) {
+        return ResponseEntity.ok(districtService.getAllDistricts());
+    }
 }

2c07795cc828864d2a9b04a1d0fd700776241a5c
nassaigael <hei.nassai@gmail.com>
2025-12-16 20:12:58 +0300

    add DistrictRepo classe on package repository:: Interface content request

--- api/src/main/java/mg/fizanakara/api/repository/DistrictRepository.java
+++ api/src/main/java/mg/fizanakara/api/repository/DistrictRepository.java
@@ -1,4 +1,11 @@
 package mg.fizanakara.api.repository;
 
-public interface DistrictRepository {
+import mg.fizanakara.api.models.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DistrictRepository extends JpaRepository<District, Integer> {
+    Optional<District> findByName(String name);
+    boolean existsByName(String name);
 }

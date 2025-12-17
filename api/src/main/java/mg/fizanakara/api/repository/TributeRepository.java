ae375eeb888e4d46a7fbe1e62ae289d32aed736e
nassaigael <hei.nassai@gmail.com>
2025-12-17 10:21:47 +0300

    created TributeRepo to cout Method

---
+++ api/src/main/java/mg/fizanakara/api/repository/TributeRepository.java
@@ -0,0 +1,12 @@
+package mg.fizanakara.api.repository;
+
+import mg.fizanakara.api.models.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TributeRepository extends JpaRepository<District, Long> {
+    Optional<District> findByName(String name);
+    boolean existsByName(String name);
+    Optional<District> findById(Long id);
+}

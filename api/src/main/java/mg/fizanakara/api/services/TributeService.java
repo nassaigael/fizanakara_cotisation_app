1d0527ab86e2fefaedbd7a89c5bd8d72ce8fb15c
nassaigael <hei.nassai@gmail.com>
2025-12-17 10:35:28 +0300

    Auto import

--- api/src/main/java/mg/fizanakara/api/services/TributeService.java
+++ api/src/main/java/mg/fizanakara/api/services/TributeService.java
@@ -1,5 +1,16 @@
 package mg.fizanakara.api.services;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mg.fizanakara.api.models.Tribute;
+import mg.fizanakara.api.repository.TributeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class TributeService {
     private final TributeRepository tributeRepository;
 

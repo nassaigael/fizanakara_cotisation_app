e25091e45ffc242a19183057967d471703cf2e1f
nassaigael <hei.nassai@gmail.com>
2025-12-17 11:57:39 +0300

    Auto import Map

--- api/src/main/java/mg/fizanakara/api/services/TributeService.java
+++ api/src/main/java/mg/fizanakara/api/services/TributeService.java
@@ -10,6 +10,7 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service

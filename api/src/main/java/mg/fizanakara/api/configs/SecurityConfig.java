682becb520e7f601f10fdd1569a0464929563bf3
mekill404 <hei.mahery.55@gmail.com>
2025-12-17 12:27:29 +0100

    commit majher

--- api/src/main/java/mg/fizanakara/api/configs/SecurityConfig.java
+++ api/src/main/java/mg/fizanakara/api/configs/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000",
+                "http://localhost:8080",
                 "http://localhost:5173"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));

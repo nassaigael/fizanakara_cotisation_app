4875bb9d453e3e58ee44471311f077b4611db7da
nassaigael <hei.nassai@gmail.com>
2025-12-17 18:06:57 +0300

    Auto import on MemberResponseDTO

--- api/src/main/java/mg/fizanakara/api/dto/MemberResponseDto.java
+++ api/src/main/java/mg/fizanakara/api/dto/MemberResponseDto.java
@@ -1,4 +1,13 @@
 package mg.fizanakara.api.dto;
 
+import lombok.Data;
+import mg.fizanakara.api.models.District;
+import mg.fizanakara.api.models.Tribute;
+import mg.fizanakara.api.models.enums.Gender;
+import mg.fizanakara.api.models.enums.MemberStatus;
+
+import java.time.LocalDate;
+
+@Data
 public class MemberResponseDto {
 }

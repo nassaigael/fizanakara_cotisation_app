57f2e8608387ef79c676c93dfedf67d91645675c
nassaigael <hei.nassai@gmail.com>
2025-12-17 11:11:05 +0300

    created and definite route on TributeContr

---
+++ api/src/main/java/mg/fizanakara/api/controllers/TributeController.java
@@ -0,0 +1,18 @@
+package mg.fizanakara.api.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mg.fizanakara.api.services.DistrictService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admins/tributes")
+@RequiredArgsConstructor
+@Slf4j
+public class TributeController {
+    private final DistrictService districtService;
+
+    //  🔒 ALL ROUTES IS PROTECTED BY ADMIN TOKEN
+
+}

1055c986c52fc5f640371b6739e3597e13645f28
nassaigael <hei.nassai@gmail.com>
2025-12-17 10:44:25 +0300

    Repository Annotation

--- api/src/main/java/mg/fizanakara/api/repository/DistrictRepository.java
+++ api/src/main/java/mg/fizanakara/api/repository/DistrictRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DistrictRepository extends JpaRepository<District, Long> {
     Optional<District> findByName(String name);
     boolean existsByName(String name);

09f3f4cf6eb9eeddf4cd39aef90f876bdf33b48c
nassaigael <hei.nassai@gmail.com>
2025-12-17 11:56:58 +0300

    Auto import Map

--- api/src/main/java/mg/fizanakara/api/services/DistrictService.java
+++ api/src/main/java/mg/fizanakara/api/services/DistrictService.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor

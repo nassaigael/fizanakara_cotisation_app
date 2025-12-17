ee2a7fe09a62c713ccd98e322c56281e3187558f
nassaigael <hei.nassai@gmail.com>
2025-12-17 11:41:50 +0300

    Auto import HttpStatus

--- api/src/main/java/mg/fizanakara/api/controllers/TributeController.java
+++ api/src/main/java/mg/fizanakara/api/controllers/TributeController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import mg.fizanakara.api.dto.TributeDto;
 import mg.fizanakara.api.models.Tribute;
 import mg.fizanakara.api.services.TributeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;

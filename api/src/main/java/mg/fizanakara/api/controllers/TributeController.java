b895d2c5eb6bacdf6d18f6f65184332305d45de2
nassaigael <hei.nassai@gmail.com>
2025-12-17 11:32:53 +0300

    Auto import annotation

--- api/src/main/java/mg/fizanakara/api/controllers/TributeController.java
+++ api/src/main/java/mg/fizanakara/api/controllers/TributeController.java
@@ -6,9 +6,8 @@ import mg.fizanakara.api.models.Tribute;
 import mg.fizanakara.api.services.DistrictService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 

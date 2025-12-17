d188411acb347687becb8813edc3a2799cc1a9f4
nassaigael <hei.nassai@gmail.com>
2025-12-17 11:29:51 +0300

    Null of return value

--- api/src/main/java/mg/fizanakara/api/services/DistrictService.java
+++ api/src/main/java/mg/fizanakara/api/services/DistrictService.java
@@ -64,6 +64,7 @@ public class DistrictService {
     @Transactional
     public ResponseEntity<Map<String, Object>> deleteAllDistricts() {
         districtRepository.deleteAll();
+        return null;
     }
 
 }

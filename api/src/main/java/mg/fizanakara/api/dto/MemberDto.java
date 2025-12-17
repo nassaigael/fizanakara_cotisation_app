791f1c240b3652fd42258b28d8bcb0ee8b49a888
nassaigael <hei.nassai@gmail.com>
2025-12-17 18:08:43 +0300

    Added:: All Attribut on MemberDTO

--- api/src/main/java/mg/fizanakara/api/dto/MemberDto.java
+++ api/src/main/java/mg/fizanakara/api/dto/MemberDto.java
@@ -8,4 +8,13 @@ import java.time.LocalDate;
 
 @Data
 public class MemberDto {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Gender gender;
+    private String imageUrl;
+    private String phoneNumber;
+    private MemberStatus status;
+    private Long districtId;
+    private Long tributeId;
 }

72a78c0b0d17d9a8550bc8b9cadb65c4f6821373
nassaigael <hei.nassai@gmail.com>
2025-12-17 10:40:45 +0300

    Auto import of Transactionnal

--- api/src/main/java/mg/fizanakara/api/services/TributeService.java
+++ api/src/main/java/mg/fizanakara/api/services/TributeService.java
@@ -1,5 +1,6 @@
 package mg.fizanakara.api.services;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mg.fizanakara.api.models.Tribute;

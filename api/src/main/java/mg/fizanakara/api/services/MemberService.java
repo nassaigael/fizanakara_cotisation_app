6026ec65c52a653878ffae62a9023f20001766d3
nassaigael <hei.nassai@gmail.com>
2025-12-17 18:09:20 +0300

    Auto import on MemberService

--- api/src/main/java/mg/fizanakara/api/services/MemberService.java
+++ api/src/main/java/mg/fizanakara/api/services/MemberService.java
@@ -1,4 +1,23 @@
 package mg.fizanakara.api.services;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mg.fizanakara.api.dto.MemberDto;
+import mg.fizanakara.api.exceptions.MemberNotFoundException;
+import mg.fizanakara.api.models.District;
+import mg.fizanakara.api.models.Members;
+import mg.fizanakara.api.models.Tribute;
+import mg.fizanakara.api.repository.DistrictRepository;
+import mg.fizanakara.api.repository.MemberRepository;
+import mg.fizanakara.api.repository.TributeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MemberService {
 }

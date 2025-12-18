83690813b885b9cea287a0cef10a0efed2fcad7d
nassaigael <hei.nassai@gmail.com>
2025-12-18 11:48:17 +0300

    Delete until import

--- api/src/main/java/mg/fizanakara/api/repository/RefreshTokenRepository.java
+++ api/src/main/java/mg/fizanakara/api/repository/RefreshTokenRepository.java
@@ -4,7 +4,6 @@ import mg.fizanakara.api.models.Admins;
 import mg.fizanakara.api.models.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {

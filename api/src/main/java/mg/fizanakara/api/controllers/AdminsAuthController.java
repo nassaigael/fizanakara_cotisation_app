03b7f726538eccf16fcdadcd2fcc109954a9533c
nassaigael <hei.nassai@gmail.com>
2025-12-18 11:10:37 +0300

    Translate value of return in EN

--- api/src/main/java/mg/fizanakara/api/controllers/AdminsAuthController.java
+++ api/src/main/java/mg/fizanakara/api/controllers/AdminsAuthController.java
@@ -84,7 +84,7 @@ public class AdminsAuthController {
         } catch (BadCredentialsException e) {
             log.warn("Login failed for {} : Credentials invalids", req.getEmail());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(Map.of("error", "Email ou mot de passe incorrect", "success", false));
+                    .body(Map.of("error", "Email or password incorrect", "success", false));
         } catch (Exception e) {
             log.error("Erreur login pour {} : {}", req.getEmail(), e.getMessage());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)

a674ec20efe4839cf543ede4c86138cc8710e632
nassaigael <hei.nassai@gmail.com>
2025-12-18 11:00:59 +0300

    All attributes of UpdateMember DTO

--- api/src/main/java/mg/fizanakara/api/dto/UpdateMemberDto.java
+++ api/src/main/java/mg/fizanakara/api/dto/UpdateMemberDto.java
@@ -1,4 +1,13 @@
 package mg.fizanakara.api.dto;
 
 public class UpdateMemberDto {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String gender;
+    private TributeDto tribute;
+    private DistrictDto district;
+    private String imageUrl;
+    private String phoneNumber;
+    private MemberDto status;
 }

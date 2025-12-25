14f1756017344d2cd8cd1fc0ae433b9a9288c35f
mekill404 <hei.mahery.55@gmail.com>
2025-12-25 08:48:31 +0100

    feat(style): styles centalization

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const THEME =
+{
+    font:
+    {
+        black: 'font-[900] uppercase tracking-widest',
+        bold: 'font-bold tracking-tight',
+        light: 'font-light trackig-wide text-brand-muted'
+    },
+    click_effect: 'active:translate-y-1 active:border-b-0 transition-all duration-150',
+    card: 'bg-white dark:bg-brand-border-dark border-2 border-brand-border border-b-8 border-b-brand-border-dark rounded-2xl shadow-sm',
+    input: 'bg-brand-bg border-2 border-brand-border border-t-brand-border-dark rounded-2xl px-4 py-3 outline-none focus:bg-white focus:border-brand-primary text-brand-text font-bold trasition-all',
+    button_primary: `bg-brand-primary border-brand-primary-dark border-b-4 text-white font-[900] uppercase text-[11px] tracking-widest hover:brightness-110 active:translate-y-1 active:border-b-0 transition-all`,
+
+  button_secondary: `bg-white dark:bg-brand-border-dark border-brand-border border-2 border-b-4 text-brand-muted hover:text-brand-primary transition-all active:translate-y-1 active:border-b-0`,
+}

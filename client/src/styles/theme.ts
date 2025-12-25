6aec1248ca3ee783be9888b11e97e014a95f38a9
mekill404 <hei.mahery.55@gmail.com>
2025-12-25 08:49:06 +0100

    refactor(theme) remove indentation

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -10,6 +10,5 @@ export const THEME =
     card: 'bg-white dark:bg-brand-border-dark border-2 border-brand-border border-b-8 border-b-brand-border-dark rounded-2xl shadow-sm',
     input: 'bg-brand-bg border-2 border-brand-border border-t-brand-border-dark rounded-2xl px-4 py-3 outline-none focus:bg-white focus:border-brand-primary text-brand-text font-bold trasition-all',
     button_primary: `bg-brand-primary border-brand-primary-dark border-b-4 text-white font-[900] uppercase text-[11px] tracking-widest hover:brightness-110 active:translate-y-1 active:border-b-0 transition-all`,
-
-  button_secondary: `bg-white dark:bg-brand-border-dark border-brand-border border-2 border-b-4 text-brand-muted hover:text-brand-primary transition-all active:translate-y-1 active:border-b-0`,
+    button_secondary: `bg-white dark:bg-brand-border-dark border-brand-border border-2 border-b-4 text-brand-muted hover:text-brand-primary transition-all active:translate-y-1 active:border-b-0`,
 }

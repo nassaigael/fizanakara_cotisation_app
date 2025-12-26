2b3d0c208ce3674206e05572974e66dac391505f
nassaigael <hei.nassai@gmail.com>
2025-12-26 13:17:46 +0300

    Added::@Transactional

--- api/src/main/java/mg/fizanakara/api/services/ChildrenService.java
+++ api/src/main/java/mg/fizanakara/api/services/ChildrenService.java
@@ -52,7 +52,8 @@ public class ChildrenService {
     }
 
     // GET BY MEMBER ID
-    public List<Children> getChildrenByMemberId(String memberId) {
+    @Transactional(readOnly = true)
+    public List<ChildrenResponseDto> getChildrenByMemberId(String memberId) {
         log.info("Retrieving children for member ID: {}", memberId);
         return childrenRepository.findByMemberId(memberId);
     }

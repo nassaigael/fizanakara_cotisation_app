6aa7a3b4cbce743b0f127bc14e0b87a999e65e5c
nassaigael <hei.nassai@gmail.com>
2025-12-26 13:15:32 +0300

    Auto import Collectors

--- api/src/main/java/mg/fizanakara/api/services/ChildrenService.java
+++ api/src/main/java/mg/fizanakara/api/services/ChildrenService.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor

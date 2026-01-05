02b5d0f3abff76f2c6e02caf466236b4b0367809
nassaigael <hei.nassai@gmail.com>
2026-01-05 22:59:15 +0300

    Added::api with all routes

--- api/src/main/java/mg/fizanakara/api/controllers/MemberController.java
+++ api/src/main/java/mg/fizanakara/api/controllers/MemberController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/admins/members")
+@RequestMapping("/api/admins/members")
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {

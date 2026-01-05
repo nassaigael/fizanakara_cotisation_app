c5bce10e2de60fbbec684a3a9eefa1be17f227c4
nassaigael <hei.nassai@gmail.com>
2026-01-05 23:00:03 +0300

    Added::api with all routes

--- api/src/main/java/mg/fizanakara/api/controllers/PaymentController.java
+++ api/src/main/java/mg/fizanakara/api/controllers/PaymentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admins/payments")
+@RequestMapping("/api/admins/payments")
 @RequiredArgsConstructor
 @Slf4j
 public class PaymentController {
